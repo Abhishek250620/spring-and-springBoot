00ec4d629276cd6e136731843ce13610c57dfb68
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-06-02 15:43:48 +0530

    Spring boot

--- SB1/src/main/java/com/example/SB1/Demo.java
+++ SB1/src/main/java/com/example/SB1/Demo.java
@@ -1,11 +1,14 @@
 package com.example.SB1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Demo {
 
+	@Autowired
+	private Demo1 demo1;
 	public void run() {
-		System.out.println("Hello word..!!!");
+		demo1.run1();
 	}
 }
---
+++ SB1/src/main/java/com/example/SB1/Demo1.java
@@ -0,0 +1,10 @@
+package com.example.SB1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Demo1 {
+	void run1() {
+		System.out.println("Running Demo1");
+	}
+}
