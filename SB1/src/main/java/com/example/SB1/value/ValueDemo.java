8c2f30dda1c293a9a9009238632f1d72723437a4
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-06-05 15:57:32 +0530

    @value added

--- SB1/src/main/java/com/example/SB1/value/ValueDemo.java
+++ SB1/src/main/java/com/example/SB1/value/ValueDemo.java
@@ -9,7 +9,20 @@ public class ValueDemo {
 	@Value("Acuver")
 	private String name;
 	
+	@Value("${mail.host}")
+	private String host;
+	
+	@Value("${mail.email}")
+	private String email;
+	
+	@Value("${mail.password}")
+	private String password;
+	
 	public void meth() {
 		System.out.println(name);
+		System.out.println(host);
+		System.out.println(email);
+		System.out.println(password);
+		
 	}
 }
